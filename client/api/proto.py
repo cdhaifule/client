19e71546e4a9bc11127f0e27b3a6ed99c4eaada7
nietschy <nietscherarek@gmail.com>
2013-12-09 13:19:57 +0100

    tt

--- client/api/proto.py
+++ client/api/proto.py
@@ -109,6 +109,7 @@ def pack_message(destination, command=None, in_response_to=None, payload=None, c
 
 
 def unpack_message(layer1):
+    print "repr:", layer1, type(layer1)
     if layer1[5] & BASE64:
         layer1[6] = base64.standard_b64decode(str(layer1[6]).replace('\n', ''))
     if layer1[5] & ENCRYPTED:
